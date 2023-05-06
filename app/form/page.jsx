7f97ce1bc905016b45f37cad00cabd5594a5310f
Dwiki Krisna Saputra <129815419+dwikiKrisna@users.noreply.github.com>
2023-05-06 06:15:30 +0000

    feat: page form input logbook

--- app/form/page.jsx
+++ app/form/page.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import SignatureCanvas from "react-signature-canvas";
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+  //text input component
+  const TextInput = ({ label, type, placeholder }) => {
+    return (
+      <div className="form-control w-full max-w-xs">
+        <label className="label">
+          <span className="label-text">{label}</span>
+        </label>
+        <input
+          type={type}
+          placeholder={placeholder}
+          className="input input-bordered w-full max-w-xs"
+        />
+      </div>
+    );
+  };
 
-export default Form
+  return (
+    <body>
+      <div className="container max-w-screen-md mx-auto my-5 p-5">
+        <Link href="/">
+          <button className="btn btn-md mb-12">Kembali</button>
+        </Link>
+        <h1 className="text-2xl font-bold mb-5">
+          Form Logbook Masuk ke Ruang Server
+        </h1>
+        <form className="flex flex-wrap gap-3">
+          <TextInput label="Nama" type="text" placeholder="Nama" />
+          <TextInput label="Institusi" type="text" placeholder="Institusi" />
+          <TextInput
+            label="Jenis Identitas"
+            type="text"
+            placeholder="NIK/NIP"
+          />
+          <TextInput label="Nomor Identitas" type="text" placeholder="" />
+          <div className="form-control ">
+            <label className="label">
+              <span className="label-text">Keperluan</span>
+            </label>
+            <textarea
+              className="textarea textarea-bordered h-24"
+              placeholder="Tulis keperluan anda masuk ke ruang server"
+            ></textarea>
+          </div>
+          <TextInput label="Tanggal" type="date" placeholder="" />
+          <TextInput label="Waktu Masuk" type="time" placeholder="" />
+          <TextInput label="Waktu Keluar" type="time" placeholder="" />
+          <TextInput label="Nama Pemberi Izin" type="text" placeholder="" />
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Ttd. Pemberi Izin</span>
+            </label>
+            <SignatureCanvas
+              penColor="black"
+              canvasProps={{ className: "w-full h-32 border rounded-lg" }}
+            />
+          </div>
+
+          <TextInput label="Nama Pendamping" type="text" placeholder="" />
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Ttd. Pendamping</span>
+            </label>
+            <SignatureCanvas
+              penColor="black"
+              canvasProps={{ className: "w-full h-32 border rounded-lg" }}
+            />
+          </div>
+        </form>
+      </div>
+    </body>
+  );
+};
+
+export default Form;
--- app/page.js
+++ app/page.js
@@ -1,21 +1,43 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen w-screen">
       <div className="flex flex-col flex-1 justify-center items-center">
         <div>
-          <h1 className="text-3xl font-bold p-2 mb-2">Catatan Akses Masuk</h1>
-          <h1 className="text-4xl font-bold  p-2">
-            Ruang Server (Data Center)
+          <h1 className=" text-3xl font-bold  mb-2 ">
+            <mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">
+              Catatan Akses Masuk
+            </mark>
           </h1>
+          <h1 className="text-4xl font-bold mb-5">
+            <mark class="px-2 text-white bg-blue-600 rounded dark:bg-blue-500">
+              Ruang Server (Data Center)
+            </mark>
+          </h1>
+
+          <Link href="/form">
+            <button className="btn btn-md ">Isi Logbook</button>
+          </Link>
         </div>
       </div>
+
       <div className="bg-green-400 flex flex-col p-5 justify-between">
-        <div className="mb-16 bg-white p-5 border-2 border-black">
-          <p className="font-bold text-center">Klasifikasi Informasi</p>
-          <p className="text-green-500 font-bold text-center">TERBUKA</p>
+        <div>
+          <div className="mb-5 bg-white p-5 border-2 border-black">
+            <p className="font-bold text-center">Klasifikasi Informasi</p>
+            <p className="text-green-500 font-bold text-center">TERBUKA</p>
+          </div>
+          <Image
+            className="mx-auto"
+            src="/Logo.png"
+            width={150}
+            height={150}
+            alt="PPATK Logo"
+          />
         </div>
+
         <div>
           <p className="text-xl font-bold mb-5">Pusat Teknologi Informasi </p>
           <p className="text-xl font-bold">PPATK</p>
